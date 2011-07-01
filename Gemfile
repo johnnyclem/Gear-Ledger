f16d00dd4a0fc96cb3d18e6a01e1f817505be728
John Clem <johnnyclem@gmail.com>
2011-07-01 12:50:07 -0700

    switched file uploads over to S3

--- Gemfile
+++ Gemfile
@@ -21,3 +21,5 @@ gem "mocha", :group => :test
 gem 'wice_grid', "3.0.0"
 
 gem "will_paginate", "3.0.pre"
+
+gem 'aws-s3'
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,14 @@ class Item < ActiveRecord::Base
   attr_accessible :package_tokens, :name, :make, :model, :barcode, :serial, :checked_out_by, :price, :condition, :last_serviced, :next_service, :photo
   has_many :packagings
   has_many :packages, :through => :packagings
-  has_attached_file :photo, :styles => { :medium => "300x300>", :thumb => "50x50>" }
+  has_attached_file :photo, 
+    :styles => { 
+      :medium => "300x300>", 
+      :thumb => "50x50>" 
+      },
+      :storage => :s3,
+      :s3_credentials => "#{RAILS_ROOT}/config/s3.yml",
+      :path => ":attachment/:id/:style.:extension"
   
   attr_reader :package_tokens
   
