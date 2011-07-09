01c2c6cb7647c8725f776ccc36728ddfd2a0edac
John Clem <johnnyclem@gmail.com>
2011-07-08 18:58:20 -0700

    secured sign-up

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Bookstore::Application.routes.draw do
   
   match '/sign_out' =>  "clearance/sessions#destroy" 
           
-  match '/sign_up' => 'items#index'
+  match '/sign_up' => '/sign_up', :constraints => { :host => /localhost/ }
   
   resources :items
 
